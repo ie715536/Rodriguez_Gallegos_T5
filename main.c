936f768ce8f62c8682b96c734b224f61223747e9
Jose Isaac Gallegos Velica <39529793+IsaacGV99@users.noreply.github.com>
2020-10-11 19:36:01 -0500

    Add time_queue managment
    The Send and Receive functions for the Queue were added in the Seconds, Minutes, Hours and Print tasks to manage the time_queue

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #include "peripherals.h"
 #include "pin_mux.h"
 #include "clock_config.h"
-#include "MK66F18.h"
+#include "MKL43Z4.h"
 #include "fsl_debug_console.h"
 /* TODO: insert other include files here. */
 #include "FreeRTOS.h"
@@ -26,11 +26,15 @@
 #define BIT_MINUTOS (1 << 1)
 #define BIT_HORAS (1 << 2)
 
+#define INIT_SECONDS	17
+#define INIT_MINUTES	33
+#define INIT_HOURS		18
+
 typedef enum
 {
-  seconds_type,
-  minutes_type,
-  hours_type
+	seconds_type,
+	minutes_type,
+	hours_type
 } time_types_t;
 
 typedef struct
@@ -61,133 +65,166 @@ QueueHandle_t Queue;
 EventGroupHandle_t Group;
 
 SemaphoreHandle_t mutex;
-
-
+/*
+ * @brief   Application entry point.
+ */
 int main(void)
 {
-  alarm_t alarm;
+	alarm_t alarm;
 
-  alarm.seconds = 0;
-  alarm.minutes = 0;
-  alarm.hours = 0;
+	alarm.seconds = 0;
+	alarm.minutes = 0;
+	alarm.hours = 0;
 
-  /* Init board hardware. */
-  BOARD_InitBootPins();
-  BOARD_InitBootClocks();
-  BOARD_InitBootPeripherals();
-#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-  /* Init FSL debug console. */
-  BOARD_InitDebugConsole();
-#endif
+  	/* Init board hardware. */
+    BOARD_InitBootPins();
+    BOARD_InitBootClocks();
+    BOARD_InitBootPeripherals();
+  	/* Init FSL debug console. */
+    BOARD_InitDebugConsole();
 
-  xTaskCreate(second_task, "Seconds", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 4, NULL);
-  xTaskCreate(minute_task, "Minutes", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 4, NULL);
-  xTaskCreate(hour_task, "Hours", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 4, NULL);
-  xTaskCreate(alarm_task, "Alarm", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 4, NULL);
-  xTaskCreate(print_task, "Print", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 4, NULL);
+    xTaskCreate(second_task, "Seconds", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 4, NULL);
+    xTaskCreate(minute_task, "Minutes", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 4, NULL);
+    xTaskCreate(hour_task, "Hours", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 4, NULL);
+    xTaskCreate(alarm_task, "Alarm", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 4, NULL);
+    xTaskCreate(print_task, "Print", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 4, NULL);
 
-  /* Force the counter to be placed into memory. */
-  volatile static int i = 0 ;
-  /* Enter an infinite loop, just incrementing a counter. */
-  Seconds = xSemaphoreCreateBinary();
-  Minutes = xSemaphoreCreateBinary();
+    Seconds = xSemaphoreCreateBinary();
+    Minutes = xSemaphoreCreateBinary();
 
-  Queue = xQueueCreate(3, sizeof(time_msg_t));
+    Queue = xQueueCreate(3, sizeof(time_msg_t));
 
-  mutex = xSemaphoreCreateMutex();
+    mutex = xSemaphoreCreateMutex();
 
-  Group = xEventGroupCreate();
+    Group = xEventGroupCreate();
 
-  while(1) {
-      i++ ;
-      /* 'Dummy' NOP to allow source level single stepping of
-          tight while() loop */
-      __asm volatile ("nop");
-  }
-  return 0 ;
+
+    /* Force the counter to be placed into memory. */
+    volatile static int i = 0 ;
+    /* Enter an infinite loop, just incrementing a counter. */
+    while(1) {
+        i++ ;
+        /* 'Dummy' NOP to allow source level single stepping of
+            tight while() loop */
+        __asm volatile ("nop");
+    }
+    return 0 ;
 }
 
 void alarm_task(void *parameters)
 {
-  const EventBits_t xBitsToWaitFor = ( BIT_SEGUNDOS | BIT_MINUTOS | BIT_HORAS );
-  EventBits_t xEventGroupValue;
+	const EventBits_t xBitsToWaitFor = ( BIT_SEGUNDOS | BIT_MINUTOS | BIT_HORAS );
+	EventBits_t xEventGroupValue;
 
-  for(;;)
-  {
+	for(;;)
+	{
 
-    xEventGroupValue = xEventGroupWaitBits(Group, xBitsToWaitFor, pdTRUE, pdTRUE, portMAX_DELAY);
+		xEventGroupValue = xEventGroupWaitBits(Group, xBitsToWaitFor, pdTRUE, pdTRUE, portMAX_DELAY);
 
-    if( (xEventGroupValue & BIT_SEGUNDOS) && (xEventGroupValue & BIT_MINUTOS) && (xEventGroupValue & BIT_HORAS)!= 0)
-    {
-		xSemaphoreTake(mutex, portMAX_DELAY);
-		PRINTF("ALARMA");
-		xSemaphoreGive(mutex);
-    }
-  }
+		if( (xEventGroupValue & BIT_SEGUNDOS) && (xEventGroupValue & BIT_MINUTOS) && (xEventGroupValue & BIT_HORAS)!= 0)
+		{
+			xSemaphoreTake(mutex, portMAX_DELAY);
+			PRINTF("ALARMA");
+			xSemaphoreGive(mutex);
+		}
+	}
 }
 
 void print_task(void *parameters)
 {
-  for(;;)
-  {
-    xSemaphoreTake(mutex, portMAX_DELAY);
-    PRINTF("Imprimir hora obtenida desde la Queue");
-    xSemaphoreGive(mutex);
-  }
+	time_msg_t incoming_msg;
+	uint8_t seconds = INIT_SECONDS;
+	uint8_t minutes = INIT_MINUTES;
+	uint8_t hours = INIT_HOURS;
+	
+	for(;;)
+	{
+		//If Queue is empty wait until its not. 
+		if(xQueueReceive( Queue, incoming_msg, portMAX_DELAY))
+		{
+			switch(incoming_msg.time_type)
+			{
+			case seconds_type:
+				seconds = incoming_msg.value;
+				break;
+			case minutes_type:
+				minutes = incoming_msg.value;
+				break;
+			case hours_type:
+				hours = incoming_msg.value;
+				break;
+			}
+			
+			xSemaphoreTake(mutex, portMAX_DELAY);
+			PRINTF("%02d : %02d : %02d\r\n", hours, minutes, seconds);
+			xSemaphoreGive(mutex);
+		}
+	}
 }
 
 
 void second_task(void *parameters)
 {
-  uint8_t counter_seconds = 0;
-  TickType_t  xLastWakeTime = xTaskGetTickCount();
+	uint8_t counter_seconds = INIT_SECONDS;
+	TickType_t  xLastWakeTime = xTaskGetTickCount();
 
-  TickType_t   xfactor = pdMS_TO_TICKS(1000);
+	TickType_t   xfactor = pdMS_TO_TICKS(1000);
 
-  while(true)
-  {
-    vTaskDelayUntil(&xLastWakeTime,xfactor);
+	while(true)
+	{
+		vTaskDelayUntil(&xLastWakeTime,xfactor);
 
-    counter_seconds++;
+		counter_seconds++;
 
-    if(counter_seconds >= 60)
-    {
-      counter_seconds = 0;
-      xSemaphoreGive(Seconds);
-    }
-  }
+		if(counter_seconds >= 60)
+		{
+			counter_seconds = 0;
+			xSemaphoreGive(Seconds);
+		}
+
+		//Send new seconds value to time_queue
+		xQueueSendToBack( Queue, &counter_seconds, portMAX_DELAY);
+	}
 }
+
 void minute_task(void *parameters)
 {
-  uint8_t counter_minutes = 0;
+	uint8_t counter_minutes = INIT_MINUTES;
 
-  while(true)
-  {
-    xSemaphoreTake(Seconds, portMAX_DELAY);
-    counter_minutes++;
+	while(true)
+	{
+		xSemaphoreTake(Seconds, portMAX_DELAY);
+		counter_minutes++;
 
-    if(counter_minutes == 60)
-    {
-      counter_minutes = 0;
+		if(counter_minutes == 60)
+		{
+			counter_minutes = 0;
 
-      xSemaphoreGive(Minutes);
-    }
+			xSemaphoreGive(Minutes);
+		}
 
-  }
+		//Send new minutes value to time_queue
+		xQueueSendToBack( Queue, &counter_minutes, portMAX_DELAY);
+
+	}
 }
+
 void hour_task(void *parameters)
 {
-  uint8_t counter_hours = 0;
+	uint8_t counter_hours = INIT_HOURS;
 
-  while(true)
-  {
-    xSemaphoreTake(Minutes, portMAX_DELAY);
-    counter_hours++;
+	while(true)
+	{
+		xSemaphoreTake(Minutes, portMAX_DELAY);
+		counter_hours++;
 
-    if(counter_hours == 24)
-    {
-      counter_hours = 0;
-    }
+		if(counter_hours == 24)
+		{
+			counter_hours = 0;
+		}
 
-  }
+		//Send new hours value to time_queue
+		xQueueSendToBack( Queue, &counter_hours, portMAX_DELAY);
+
+	}
 }
